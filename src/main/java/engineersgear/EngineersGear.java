f98e2dd01a3abe7e869a67257e455636b8ab6733
sguest <spencer.b.guest@gmail.com>
2022-05-30 12:27:52 -0500

    Mod setup with JEI and IE deps

---
+++ src/main/java/engineersgear/EngineersGear.java
@@ -0,0 +1,8 @@
+package engineersgear;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EngineersGear.MODID)
+public class EngineersGear {
+    public static final String MODID = "engineersgear";
+}
